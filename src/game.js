19ce351699b287e2ca7d921c340bf93f3cb4906c
Stephen Lloyd <stephenlloyd@Stephens-MacBook-Pro.local>
2020-06-28 18:08:32 +0100

    added swipe

--- src/game.js
+++ src/game.js
@@ -63,3 +63,52 @@ function keyDownEvent(e) {
     break;
   }
 }
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            game.changeDirection("W");
+        } else {
+          game.changeDirection("E");
+
+        }
+    } else {
+        if ( yDiff > 0 ) {
+          game.changeDirection("N");
+
+        } else {
+            game.changeDirection("S");
+
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
